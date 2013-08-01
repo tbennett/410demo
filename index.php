030050bb9eb6af796f82c9cca3b367c3ef5e982d
tbennett <betabennett@gmail.com>
2013-08-01 15:33:27 -0400

    added Database interactions
    home page content is now dynamic

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+	require_once('includes/config.php');
+	
+	//query the database and store the results
+	//in the $myData variable
+	$sql = 'SELECT * FROM site_content';
+	$myData = $db->query($sql);
+	
+	while($row = $myData->fetch_assoc())
+	{
+		if($row['view'] == 'n')
+		{
+			continue;
+		}
+		
+		if($row['section_name'] == 'intro')
+		{
+			$intro = $row['content'];
+		}
+		
+		if($row['section_name'] == 'blurb')
+		{
+			$blurb = $row['content'];
+		}
+	}
+
+?>
+
+
+
 <?php $page = "home"; ?>
 
 <?php require_once('includes/top.inc.php'); ?>
@@ -13,9 +43,8 @@
     	<img src="i/bruce.jpg" alt="Bruce Lawson is awesome!" />
     	<figcaption>According to Bruce Lawson, the section tag is NOT another DIV.</figcaption>
     </figure>
-    <p>In HTML 5 you can specifically mark up all the “secondary” content on a page such as navigation, branding, copyright notices, so it feels odd that you can’t specifically mark up the most important part of your page—the content.</p>
-<p>
-But what would be the purpose of marking it up specifically, anyway? If you need to style it, use a div. An assistive technlogy like a screenreader can find the main content because it is the first thing inside a page that isn’t a header, nav or footer.</p>
+    <p><?php echo @$intro; ?></p>
+	<p><?php echo @$blurb; ?></p>
   </section>
   
   <?php include_once('includes/sidebar.inc.php');?>
